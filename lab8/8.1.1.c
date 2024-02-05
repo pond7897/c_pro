d76d9f5c0639ebc18250214db6e1df59ce659e7c
pond7897 <80212418+pond7897@users.noreply.github.com>
2024-02-05 10:49:50 +0700

    Add files via upload

---
+++ lab8/8.1.1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main(){
+	int x = 10;
+	while (x > 1){
+		printf("%d\n",x);
+		x -= 2;
+	}
+}
---
+++ lab8/8.1.2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main(){
+	int counter = 0;
+	while(counter < 5){
+		printf("life is beautiful\n");
+		counter++;
+	}
+}
